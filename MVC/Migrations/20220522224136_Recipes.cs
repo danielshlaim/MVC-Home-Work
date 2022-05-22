f5beff48b80b3edbe019b4860c157615a0ec97a6
daniel shlaim <dada373@gmail.com>
2022-05-23 02:58:51 +0300

    Add project files.

---
+++ MVC/Migrations/20220522224136_Recipes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC.Migrations
+{
+    public partial class Recipes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CakeName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RecipeContents = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipes");
+        }
+    }
+}
---
+++ MVC/Models/Recipes.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC.Models
+{
+    public class Recipes
+    {
+        public int Id { get; set; }
+        public string CakeName { get; set; }
+        public string RecipeContents { get; set; }
+    }
+}
